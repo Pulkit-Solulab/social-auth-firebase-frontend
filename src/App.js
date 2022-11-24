a30c5ad45c89c1aad6d8cc6b0f41f296db7daee4
pulkit-solulab <pulkit@solulab.com>
2022-11-24 11:04:03 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import './App.css';
-import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut, FacebookAuthProvider, TwitterAuthProvider } from 'firebase/auth';
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut, createUserWithEmailAndPassword, FacebookAuthProvider, TwitterAuthProvider, signInWithCustomToken } from 'firebase/auth';
 import { useEffect, useState } from 'react';
 import ListOfTodo from './components/ListOfTodo';
+import axios from 'axios';
 
 function App() {
   const [auth, setAuth] = useState(false);
   const [token, setToken] = useState('');
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const code = urlParams.get('code')
 
   useEffect(() => {
-    onAuthStateChanged(getAuth(), userCred => {
-      console.log('authstatechanged')
-      if (userCred) {
-        setAuth(true);
-        userCred.getIdToken().then(token => {
-          setToken(token);
-        });
-      }
-      else setAuth(false);
-    });
+    if (code) {
+      console.log('code = ', code);
+      axios.post('http://localhost:5000/api/v1/auth/insta-exchange-code', {
+        code: code,
+        ll: 'kk',
+      }).then((response) => {
+        console.log(response.data);
+        const { access_token, user_id, customTokenFirebase } = response.data;
+        const auth = getAuth();
+        signInWithCustomToken(auth, customTokenFirebase)
+          .then((userCredential) => {
+            // Signed in
+            const user = userCredential.user;
+            console.log('user yo ', user)
+            // ...
+          })
+          .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ...
+          });
+      });
+    } else {
+      onAuthStateChanged(getAuth(), userCred => {
+        console.log('authstatechanged', userCred)
+        if (userCred) {
+          setAuth(true);
+          userCred.getIdToken().then(token => {
+            console.log('authstatechanged token', token)
+            setToken(token);
+          });
+        }
+        else setAuth(false);
+      });
+    }
+
   }, []);
 
   const logout = () => {
@@ -35,26 +65,21 @@ function App() {
     const auth = getAuth();
     const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider).then(res => {
-      if (res) setAuth(true);
+      console.log('loginWithGoogle', res)
     }).catch(err => {
       console.log(err)
     });
   }
 
-  const loginWithFacebook = () => {
-    const auth = getAuth();
-    const provider = new FacebookAuthProvider();
-    signInWithPopup(auth, provider).then(res => {
-      if (res) setAuth(true);
-    }).catch(err => {
-      console.log(err)
-    });
+  const loginWithInstagram = () => {
+    window.open("https://api.instagram.com/oauth/authorize?client_id=631080548750356&redirect_uri=https://google.com/&scope=user_profile,user_media&response_type=code", "_self");
   }
 
   const loginWithTwitter = () => {
     const auth = getAuth();
     const provider = new TwitterAuthProvider();
     signInWithPopup(auth, provider).then(res => {
+      console.log('loginWithTwitter', res)
       if (res) setAuth(true);
     }).catch(err => {
       console.log(err)
@@ -68,7 +93,7 @@ function App() {
         <ListOfTodo token={token} />
       </div> : <div>
         <button onClick={loginWithGoogle}>Login Google</button>
-        <button onClick={loginWithFacebook}>Login Facebook</button>
+        <button onClick={loginWithInstagram}>Login Instagram</button>
         <button onClick={loginWithTwitter}>Login Twitter</button>
       </div>}
     </div>
